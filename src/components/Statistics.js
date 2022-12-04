2c87d6dbe58f51b09f98573c2d4a02ba0a4768bf
Jesse Plym <bgu254@myy.haaga-helia.fi>
2022-12-04 14:10:13 +0200

    few lodash functions made

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,10 +1,31 @@
+import _ from "lodash";
 import { useEffect, useState } from "react";
 import { BarChart, XAxis, YAxis, Bar } from "recharts";
 import { API_URL_GETTRAINING } from "../constants";
+import Spinner from "./Spinner";
 
 export default function Statistics() {
   const [activities, setActivities] = useState([]);
   
+  const chartData = [];
+  // Groups all activities by activity name and sets object to data variable
+  const data = _.groupBy(activities, "activity");
+  const durations = [];
+  const activityNames = Object.keys(data);
+  // loops through each item in data array and adds all durations to own array got from sumBy function 
+  Object.entries(data).forEach((item) => {
+    durations.push((_.sumBy(item[1], i => i.duration)));
+  });
+  //console.log(data);
+  console.log(durations);
+  console.log(activityNames);
+  
+  // adds durations and activitynames to chartData by looping.
+  // values in durations and activityNames are in corresponding order
+  for (let i = 0; i < activityNames.length; i++) {
+    chartData.push({activity: activityNames[i], duration: durations[i]});
+  }
+
   useEffect(() => {
     getActivities();
   }, [])
@@ -19,7 +40,7 @@ export default function Statistics() {
     .then(data => {
       setActivities(data.map((d) => {
         return (
-          {name: d.activity, duration: d.duration}
+          {activity: d.activity, duration: d.duration}
         );
       }))
 
@@ -29,13 +50,13 @@ export default function Statistics() {
 
   return (
     <>
-      {activities.length !== 0 && 
-      <BarChart width={1100} height={300} data={activities}>
-        <XAxis dataKey="name"/>
+    { chartData.length === 0 ? <Spinner /> :
+      <BarChart width={1100} height={300} data={chartData}>
+        <XAxis dataKey="activity"/>
         <YAxis />
         <Bar dataKey="duration" fill="#8884d8"/>
-      </BarChart>}
-      
+      </BarChart>
+      }
     </>
   );
 }
